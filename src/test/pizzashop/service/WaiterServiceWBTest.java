0c2366bccba8963c6d6521bf2cc8f42de355aa6e
iuliathira <38749784+iuliathira@users.noreply.github.com>
2020-04-15 08:01:46 +0300

    [iulia]_commit
    new tests added

--- src/test/pizzashop/service/WaiterServiceWBTest.java
+++ src/test/pizzashop/service/WaiterServiceWBTest.java
@@ -11,6 +11,7 @@ import pizzashop.model.PaymentType;
 import pizzashop.repository.MenuRepository;
 import pizzashop.repository.PaymentRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,9 +37,30 @@ class WaiterServiceWBTest {
     public void test_F02_parameters_valid(PaymentType type){
 
         List<Payment> l = waiterService.getPayments();
+
         try{
             double total = waiterService.getTotalAmount(type,l);
             assertTrue(total > 0);
+        }catch(Exception e){
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @DisplayName("WBT_valid - paymentType and payment list are valid")
+    @ParameterizedTest
+    @EnumSource(value = PaymentType.class, names = "CASH", mode = EnumSource.Mode.MATCH_ANY)
+    public void test_F02_parameters_valid_02(PaymentType type){
+
+        List<Payment> l = new ArrayList<>();
+        l.add(new Payment(6,PaymentType.CASH,43.00));
+        l.add(new Payment(1,PaymentType.CARD,12.00));
+        l.add(new Payment(5,PaymentType.CASH,42.00));
+        l.add(new Payment(1,PaymentType.CARD,74.00));
+
+        try{
+            double total = waiterService.getTotalAmount(type,l);
+            assertTrue(total == 85.00);
         }catch(Exception e){
             e.printStackTrace();
             fail();
@@ -51,6 +73,21 @@ class WaiterServiceWBTest {
 
         List<Payment> l = waiterService.getPayments();
         assertThrows(ServiceException.class,()->waiterService.getTotalAmount(null,l));
+
+    }
+
+    @DisplayName("WBT_invalid - paymentList is invalid")
+    @ParameterizedTest
+    @EnumSource(value = PaymentType.class, names = "CARD", mode = EnumSource.Mode.MATCH_ANY)
+    public void test_F02_paymentList_invalid_null(PaymentType type) {
+            List<Payment> l = new ArrayList<>();
+        try {
+            double total = waiterService.getTotalAmount(type, l);
+            assertTrue(total == 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
 
     }
 
